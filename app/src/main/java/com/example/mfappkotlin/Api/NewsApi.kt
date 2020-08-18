f3afad3f6a9fa4c0a2a823194c55dc02ddd806d5
alekseevvv <alekseev.vladimir1212@gmail.com>
2020-08-18 19:41:26 +0300

    Connect to Api

--- app/src/main/java/com/example/mfappkotlin/Api/NewsApi.kt
+++ app/src/main/java/com/example/mfappkotlin/Api/NewsApi.kt
@@ -1,7 +1,8 @@
 package com.example.mfappkotlin.Api
 
 import com.example.mfappkotlin.Model.NewsModel
-import retrofit2.Response
+import io.reactivex.Single
+import okhttp3.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,11 +10,21 @@ import retrofit2.http.Query
 const val API_KEY = "b74de9f058fd49fda341fbc1d9c9bc67"
 const val BASE_URL = "https://newsapi.org/v2/"
 
+/*companion object {
+    val instance : NewsApiClient by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        retrofit.create(NewsApiClient::class.java)
+    }
+}*/
+
 interface NewsApi {
     @GET("/v2/top-headlines")
     fun getTopHeadlines(
-        @Query("country") country : String = "ru",
-        @Query("apiKey") apiKey : String ): Response<NewsModel>
-
+        @Query("country") country : String,
+        @Query("apiKey") apiKey : String ): Single<NewsModel>
 
 }
--- app/src/main/java/com/example/mfappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/mfappkotlin/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.mfappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import com.example.mfappkotlin.Api.NewsApi
+import androidx.lifecycle.MutableLiveData
+import com.example.mfappkotlin.Api.NewsAPIService
+import com.example.mfappkotlin.Model.Article
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kodein.di.Copy.Companion.invoke
 import org.kodein.di.Kodein.Companion.invoke
@@ -21,11 +28,35 @@ class MainActivity : AppCompatActivity() {
 
     val clickListener = View.OnClickListener { view->
         textView3.text = "Рбаотет,бля"
+        getDataApı()
     }
 
 
+    private val newsApiService = NewsAPIService()
+    private val disposable = CompositeDisposable()
+    var news = MutableLiveData<List<Article>>()
 
 
+    private fun getDataApı() {
+
+        disposable.add(
+            newsApiService.getNewsApi()
+                .getTopHeadlines("ru", "b74de9f058fd49fda341fbc1d9c9bc67")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        news.value = it.articles
+                        Log.d("API", it.toString())
+                    },
+                    {
+                        Log.d("API", it.message)
+                    }
+                )
+        )
+
+
+    }
 
 
 }
